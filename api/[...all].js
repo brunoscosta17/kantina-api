0727d44476a995628844218b1c488fa85d7885ca
Bruno Costa <tp.b.silva.costa@totvspartners.com.br>
2026-03-01 11:11:52 -0300

    fix: restructure Vercel API routing to use catch-all wildcard

--- api/[...all].js
+++ api/[...all].js
@@ -3,10 +3,10 @@ try {
   module.exports = handler;
 } catch (error) {
   module.exports = (req, res) => {
-    res.status(500).json({ 
-      error: 'Failed to load handler', 
+    res.status(500).json({
+      error: 'Failed to load handler',
       message: error.message,
-      stack: error.stack 
+      stack: error.stack
     });
   };
 }
